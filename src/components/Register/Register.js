169b715fee411f375dd79285b02b6cdfa99d5627
saini-sanjay <sanjaynitj4066@gmail.com>
2018-10-07 14:43:49 +0530

    kuch nhi

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -18,7 +18,7 @@ class Register extends React.Component {
     this.setState({name:event.target.value});
   };
   onSubmitSignIn=()=>{
-    fetch('http://localhost:3002/register',{
+    fetch('https://hello-sanjay.herokuapp.com/register',{
       method:'post',
       headers:{'Content-Type':'application/json'},
       body:JSON.stringify({
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -14,7 +14,7 @@ class SignIn extends React.Component{
     this.setState({signInPassword:event.target.value})
   }
   onSubmitSignIn=()=>{
-   fetch('http://localhost:3002/signin',{
+   fetch('https://hello-sanjay.herokuapp.com/signin',{
            method:'post',
       headers:{'Content-Type':'application/json'},
       body:JSON.stringify({
